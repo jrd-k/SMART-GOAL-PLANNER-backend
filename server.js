acc95c7181b22b2ae86edf880463174abd5b9dea
jared kiprop <jaredassociates@gmail.com>
2025-07-20 23:05:02 +0300

    Fix json-server version and add type module

--- server.js
+++ server.js
@@ -1,22 +1,14 @@
 // server.js
-import jsonServer from "json-server";
-import path from "path";
-import { fileURLToPath } from "url";
+const jsonServer = require("json-server");
+const path = require("path");
 
 const server = jsonServer.create();
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// Set default middlewares (logger, CORS, etc)
-server.use(jsonServer.defaults());
-
-// Set up the db.json path
 const router = jsonServer.router(path.join(__dirname, "db.json"));
+server.use(jsonServer.defaults());
 server.use(jsonServer.bodyParser);
 server.use("/goals", router);
 
-// Start server
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on port ${PORT}`);
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
 });
